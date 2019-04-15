3c9eef02e1ed8f79a62a226f22e27fdac35c4d6b
flaying <1209802304@qq.com>
2019-04-15 09:14:51 +0800

    second

---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+const gulp = require('gulp');
+const server = require('gulp-webserver');
+const connect = require('gulp-connect');
+const proxy = require('http-proxy-middleware');
+const watch = require('gulp-watch');
+const mini = require('gulp-clean-css');
+const script = require('gulp-uglify');
+
+
+//压缩css
+gulp.task('minicss',()=>{
+    gulp.src('./src/css/*.css')
+    .pipe(mini())
+    .pipe(gulp.dest('./dist/css'))
+})
+//压缩js
+
+gulp.task('minijs',()=>{
+    gulp.src('./src/js/*.js')
+    .pipe(script())
+    .pipe(gulp.dest('./dist/js'))
+})
+
+//服务开启
+
+gulp.task('myserver',()=>{
+    gulp.src('.')
+    .pipe(server({
+        host:'localhost',
+        port:3000,
+        livereload:true,
+        middleware:(req,res)=>{
+            if(req.url === '/api'){
+                res.writeHead(200,{
+                    'content-type':'text/plain;charset:utf8',
+                    'access-control-allow-origin':'*',
+                })
+                res.end()
+            }
+        }
+    }))
+})
